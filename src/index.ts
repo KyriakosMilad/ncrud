3bce65fa44742bbd3faa0392de3430a6f63bce8f
Kyriakos Milad <contact.kyriakos@gmail.com>
2020-12-21 19:54:12 +0200

    Check if command exist

--- src/index.ts
+++ src/index.ts
@@ -10,17 +10,14 @@ console.log('opts', process.argv);
 ncrud
 	.version('0.0.1')
 	.helpOption('-h, --help', 'display help for ncrud')
-	.description(
-		'Make CRUD files and stop wasting time doing same things'
-	)
+	.description('Make CRUD files and stop wasting time doing same things')
 	.option(
 		'-m, --module <name>',
 		"create module and it's schema, routes, dto(s) and controller"
 	)
 	.option('-c, --controller <name>', 'create controller')
 	.option('-s, --schema <name>', 'create schema')
-	.option('-r, --route <name>', 'create route')
-	.option('-d, --dto <name>', 'create dto')
+	.option('-r, --routes <name>', 'create routes')
 	.parse(process.argv);
 
 if (!process.argv.slice(2).length) {
@@ -28,4 +25,11 @@ if (!process.argv.slice(2).length) {
 		chalk.red(figlet.textSync('ncrud', { horizontalLayout: 'full' }))
 	);
 	ncrud.outputHelp();
+}
+
+if (
+	process.argv.slice(2).length &&
+	process.argv.slice(2)[0].split('')[0] !== '-'
+) {
+	console.log(`error: unknown command '${process.argv.slice(2)[0]}'`);
 }
