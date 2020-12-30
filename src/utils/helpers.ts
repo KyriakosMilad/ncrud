d172f705d1f42cc256cd9b058777ce860a9d96ab
Kyriakos Milad <contact.kyriakos@gmail.com>
2020-12-30 05:13:33 +0200

    Check if file exist and ask to overwrite

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,6 +1,30 @@
+import fs from 'fs';
+import inquirer from 'inquirer';
+
 export const errorHandler = async (
 	error: string,
-	code: number
+	code: string
 ): Promise<void> => {
 	console.log(`ncrud error ${code}: ${error}`);
+};
+
+export const checkFileExistAndAskToOverwrite = async (
+	uri: string
+): Promise<boolean> => {
+	try {
+		if (fs.existsSync(uri)) {
+			const askToOverwriteAnswer = await inquirer.prompt([
+				{
+					name: 'overwrite',
+					type: 'confirm',
+					message: `${uri} already exist, overwrite it?`,
+				},
+			]);
+			return askToOverwriteAnswer.overwrite;
+		} else {
+			return true;
+		}
+	} catch {
+		return false;
+	}
 };
