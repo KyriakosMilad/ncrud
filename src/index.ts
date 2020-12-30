5401b3de1b0506b1a02004f8b8749c7b0d301f05
Kyriakos Milad <contact.kyriakos@gmail.com>
2020-12-30 05:32:13 +0200

    Initialize ncrud and create ncrudconfig.json file

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,10 @@
 import ncrud from 'commander';
 import chalk from 'chalk';
 import figlet from 'figlet';
-import pluralize from 'pluralize';
 import { createController } from './utils/createController';
+import inquirer from 'inquirer';
+import { errorHandler, checkFileExistAndAskToOverwrite } from './utils/helpers';
+import fs from 'fs';
 
 // dev
 console.log('opts', process.argv);
@@ -13,6 +15,7 @@ ncrud
 	.version('0.0.1')
 	.helpOption('-h, --help', 'display help for ncrud')
 	.description('Make CRUD files and stop wasting time doing same things')
+	.option('-i, --init', 'create ncrud config')
 	.option(
 		'-m, --module <name>',
 		"create module and it's schema, routes, dto(s) and controller"
@@ -35,7 +38,7 @@ if (
 	process.argv.slice(2).length &&
 	process.argv.slice(2)[0].split('')[0] !== '-'
 ) {
-	console.log(`error: unknown command '${process.argv.slice(2)[0]}'`);
+	errorHandler(`unknown command '${process.argv.slice(2)[0]}'`, '001');
 }
 
 // check if multiple options wrote in wrong way
@@ -45,10 +48,11 @@ if (
 	process.argv.slice(2)[0].split('')[1] !== '-' &&
 	process.argv.slice(2)[0].length > 2
 ) {
-	console.log(
-		`error: unknown command ${
+	errorHandler(
+		`unknown command ${
 			process.argv.slice(2)[0]
-		} multiple options usage: "ncrud -<first option> <first option value> -<second option> <second option value>"`
+		} multiple options usage: "ncrud -<first option> <first option value> -<second option> <second option value>"`,
+		'002'
 	);
 
 	// set options to null
@@ -58,18 +62,71 @@ if (
 	ncrud.schema = '';
 }
 
+// initialize ncrud and create ncrudconfig.json file
+async function init(): Promise<void> {
+	try {
+		const overwriteNcrudConfig = await checkFileExistAndAskToOverwrite(
+			'ncrudconfig.json'
+		);
+
+		// exit program if refused to overwrite or error happened
+		if (!overwriteNcrudConfig) process.exit();
+
+		const ncrudConfigAnswers = await inquirer.prompt([
+			{
+				name: 'rootDir',
+				type: 'input',
+				message: 'Where to create files?',
+				default: '/src',
+			},
+			{
+				name: 'folderStructure',
+				type: 'list',
+				message: 'Preferred folder structure:',
+				choices: [
+					{
+						name: 'separated',
+						value: 'separated',
+					},
+					{
+						name: 'grouped',
+						value: 'grouped',
+					},
+				],
+			},
+		]);
+		if (ncrudConfigAnswers) {
+			fs.writeFileSync(
+				'ncrudconfig.json',
+				JSON.stringify(ncrudConfigAnswers, null, 2)
+			);
+		}
+
+		process.exit();
+	} catch {
+		errorHandler("can't create ncrudconfig.json file!", '003');
+	}
+}
+
+if (ncrud.init) {
+	async function i(): Promise<void> {
+		await init();
+	}
+	i();
+}
+
 // handle commands
-async function executeCommands() {
+async function executeCommands(): Promise<void> {
 	if (ncrud.controller) {
-		await createController(pluralize(ncrud.controller));
+		await createController(ncrud.controller);
 	}
 
 	// if (ncrud.routes) {
-	// 	await createRoutes(pluralize(ncrud.routes));
+	// 	await createRoutes(ncrud.routes);
 	// }
 
 	// if (ncrud.schema) {
-	// 	await createSchema(pluralize(ncrud.schema));
+	// 	await createSchema(ncrud.schema);
 	// }
 
 	// if (ncrud.module) {
